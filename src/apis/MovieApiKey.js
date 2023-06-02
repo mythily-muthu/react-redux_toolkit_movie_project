8e25b00bec606e5da1dfc93b1fd54b3e12c15d1b
mythily-muthu <mythilimuthu18@gmail.com>
2023-06-02 13:07:58 +0530

    Components created

---
+++ src/apis/MovieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKEY = "35be59cb";
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        " primary-color": " #0f171e",
+        "secondary-color": "#1a242f",
+        "font-primary": "#ffffff",
+        "font-secondary": "#79b8f3",
+      }
+    },
   },
   plugins: [],
 }
