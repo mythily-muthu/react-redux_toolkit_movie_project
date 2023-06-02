7911438f9c767374513af76cfa6a68bdd2960a45
mythily-muthu <mythilimuthu18@gmail.com>
2023-06-02 17:31:46 +0530

    Added store

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MovieListing from "../components/MovieListing";
+import { APIKEY } from "../apis/MovieApiKey";
+import movieApi from "../apis/movieApi";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../features/movies/movieSlice";
 const Home = () => {
+  let movieText = "horror";
+  const dispatch = useDispatch();
+  const getMovies = async () => {
+    let res = await movieApi
+      .get(`?apikey=${APIKEY}&s=${movieText}&type=movie`)
+      .catch((err) => {
+        console.log("error:", err);
+      });
+
+    dispatch(addMovies(res.data));
+  };
+
+  useEffect(() => {
+    getMovies();
+  });
+
   return (
     <div>
       <div></div>
