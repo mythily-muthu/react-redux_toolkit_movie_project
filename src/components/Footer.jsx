513d41597876e842263c055b05c90b6284754eb5
mythily-muthu <mythilimuthu18@gmail.com>
2023-06-04 21:10:21 +0530

    fix: changed height for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className=" flex w-full h-16 font-normal cursor-pointer text-fontprimary bg-secondary items-center gap-6 justify-center px-8">
+    <div className=" flex w-full h-14 font-normal cursor-pointer text-fontprimary bg-secondary items-center gap-6 justify-center px-8">
       <p className="text-lg ">Movie App</p>
 
       <div>©2021, Movie, Inc. or its affiliates</div>
